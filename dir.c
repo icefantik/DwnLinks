2c214ecb12b7c6249f815a4eb4c818f85f7c568b
Peter <mityushin_petya@mail.ru>
2022-02-24 13:41:40 -0800

    selc file concat

--- dir.c
+++ dir.c
@@ -11,7 +11,8 @@ void selcFile(char *file_exten, char *dir_links)
 	char *links_dir = (char*)malloc(MAXDIR * sizeof(char));
 	char *path = (char*)malloc(MAXDIR * sizeof(char));
 	getcwd(cwd, MAXDIR);
-	sprintf(links_dir, "%s/%s", cwd, DIRLINKS);
+	size_t needed_size_dir = snprintf(NULL, 0,"%s/%s", cwd, dir_links);
+	snprintf(links_dir, needed_size_dir,"%s/%s", cwd, dir_links);
 	if ((dir = opendir(links_dir)) != NULL) {
 		while ((ent = readdir(dir)) != NULL) {
 			sprintf(path, "%s/%s", links_dir, ent->d_name);
